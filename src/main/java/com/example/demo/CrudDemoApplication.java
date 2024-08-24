55fdcdfd5c5507da5a12bd5a878b47566bfd36a1
Priyam <priyamchoudhury1996@gmail.com>
2024-08-24 17:53:47 +0530

    changes

--- src/main/java/com/example/demo/CrudDemoApplication.java
+++ src/main/java/com/example/demo/CrudDemoApplication.java
@@ -8,13 +8,7 @@ public class CrudDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudDemoApplication.class, args);
-		System.out.println("Hello World");
-
-		System.out.println("KARTHIK-Branch");
-		
-		
-
-		System.out.println("MASTER BRANCH");
+	
 
 	}
 
